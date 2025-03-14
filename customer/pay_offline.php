d59f3063d6d85fd0b2d589c27ce84948aca4f175
HoangDai02 <dai.hoang668@gmail.com>
2025-03-14 23:35:02 +0700

    firstcommit

---
+++ customer/pay_offline.php
@@ -0,0 +1,26 @@
+
+<div class="mainTable">
+    <h3 class="section-title"><b>Thanh toán</b><span>ngoại tuyến</span></h3>
+    <table>
+        <!--thead-->
+        <thead>
+    
+            <tr>
+                <th colspan="4">Chi Tiết Tài Khoản Ngân Hàng</th>
+                <th colspan="4">Visa/MasterCard</th>
+            </tr>
+    
+        </thead>
+        <!--end thead-->
+    
+        <!--tbody-->
+        <tbody>
+            <tr>
+                <td colspan="4">NGO NGUYEN THANH TAM <br> Tên ngân hàng: VIETCOMBANK <br>Số tài khoản: 9974948710</td>
+                <td colspan="4">Số tài khoảng 9704 0365 4618 2928 <br> Mobile No: 0974948710 <br> Name: NGO NGUYEN THANH TAM</td>
+            </tr>
+        </tbody>
+
+    </table>
+    
+</div>
